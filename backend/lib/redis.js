4dcf3a8da4bbecd2141e4d78e3ab41941285217e
piyusshkumar <piyusshkumar9599@gmail.com>
2026-07-13 18:11:08 +0530

    added product routes and controller

--- backend/lib/redis.js
+++ backend/lib/redis.js
@@ -1,3 +1,6 @@
 import Redis from "ioredis"
- 
+import dotenv from "dotenv";
+
+dotenv.config();
+console.log("REDIS_URL:", process.env.REDIS_URL);
 export const redis = new Redis(process.env.REDIS_URL);
