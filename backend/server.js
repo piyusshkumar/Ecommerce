56b8d2f2398ba65cb1e7019f9e538a6f3920f9f3
piyusshkumar <piyusshkumar9599@gmail.com>
2026-07-14 15:10:49 +0530

    created order model

--- backend/server.js
+++ backend/server.js
@@ -25,7 +25,7 @@ app.use(cookieParser());
 app.use("/api/auth", authRoutes);
 app.use("/api/products", productRoutes);
 app.use("/api/cart", cartRoutes);
-//app.use("/api/coupons", couponRoutes);
+app.use("/api/coupons", couponRoutes);
 //app.use("/api/payments", paymentRoutes);
 //app.use("/api/analytics", analyticsRoutes);
 
