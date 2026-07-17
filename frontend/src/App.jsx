1ca180d1e3dbc620a2460be6624bc9033f848ea8
piyusshkumar <piyusshkumar9599@gmail.com>
2026-07-17 18:16:19 +0530

    added home page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import { Routes, Route } from 'react-router-dom'
 import  Signup  from './pages/Signup'
 import  Login  from './pages/Login'
+import HomePage from './pages/HomePage'
 import Navbar from './components/Navbar'
 
 
@@ -23,7 +24,7 @@ return (
 				
 					<Route path='/signup' element={ <Signup />} />
 					<Route path='/login' element={ <Login />} />
-					
+					<Route path='/' element={ <HomePage />} />
 					
 				</Routes>
 			</div>
