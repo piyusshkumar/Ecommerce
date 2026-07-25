2550e23c0897ce794997c7b624002d67463c7422
piyusshkumar <piyusshkumar9599@gmail.com>
2026-07-25 23:24:05 +0530

    added cataegory image sample

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -3,7 +3,7 @@ import CategoryItem from "../components/CategoryItem";
 
 
 const categories = [
-	{ href: "/jeans", name: "Jeans", imageUrl: "/jeans.jpg" },
+	{ href: "/jeans", name: "Jeans", imageUrl: "/jeans.avif" },
 	{ href: "/t-shirts", name: "T-shirts", imageUrl: "/tshirts.jpg" },
 	{ href: "/shoes", name: "Shoes", imageUrl: "/shoes.jpg" },
 	{ href: "/glasses", name: "Glasses", imageUrl: "/glasses.png" },
